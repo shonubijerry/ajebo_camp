1164b31acef841eef54e50355adc4b7f00e446ee
shonubijerry <korede@verifi.ng>
2026-01-23 22:21:57 +0000

    allow workers dev url

--- api/src/config/cors.ts
+++ api/src/config/cors.ts
@@ -2,7 +2,11 @@ import { cors } from 'hono/cors'
 
 export const corsConfig = cors({
   // make localhost port take any value in port
-  origin: ['http://localhost:3000', 'http://localhost:8787'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:8787',
+    'https://ajebo_camp_ui-production.koredujar.workers.dev',
+  ],
   // allowHeaders: ['Content-Type', 'Upgrade-Insecure-Requests', 'Authorization'],
   allowMethods: ['POST', 'GET', 'OPTIONS', 'PATCH', 'DELETE'],
   exposeHeaders: ['Content-Length'],
